a1f2c8acdb019d409125130ec95fec5c2d78d7eb
Kimsw-G <kimsw3445@naver.com>
2022-11-28 18:09:44 +0900

    studying vue3

---
+++ src/views/1_databinding/DataBindingHtmlView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{htmlString}}</div>
+    <div v-html="htmlString"></div>
+</template>
+<script>
+export default {
+    components: {},
+    data() {
+        return {
+            htmlString: '<p style="color:red;">빨간맛</p>'
+        }
+    },
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {}
+}
+</script>
---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>HI {{userName}}</h1>
+    <p>{{message}}</p>
+</div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            userName : "Kim SeongWuk",
+            message : 'Hello World!!',
+            arr : [],
+            obj : {}
+        }
+    }
+}
+</script>
+
+<style scoped> 
+
+</style>
